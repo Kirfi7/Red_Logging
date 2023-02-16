fc48915f73866f4818ff46f66bc782ef4152ca8b
Pearson77 <mialal0505@gmail.com>
2023-02-16 20:07:37 +0300

    2.0

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+import time
+import vk_api
+import sqlite3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from cfg import TOKEN, DEV, STAFF, CONST
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+vk_session = vk_api.VkApi(token=TOKEN)
+lp = VkBotLongPoll(vk_session, 218860473)
+vk = vk_session.get_api()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def sender(for_chat_id, message_text):
+    vk_session.method("messages.send", {
+        "chat_id": for_chat_id,
+        "message": message_text,
+        "random_id": 0
+    })
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    try:
+        for event in lp.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat:
+                chat_id = event.chat_id
+                text = event.object.message['text']
+                user_id = event.object.message['from_id']
+                message_id = event.object.message['conversation_message_id']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+                if chat_id == CONST and "Зашёл" in text:
+                    pass
+
+                elif chat_id == CONST and "Перезахожу" in text:
+                    pass
+
+                elif chat_id == CONST and "Вышел" in text:
+                    pass
+
+                elif str(user_id) in DEV and text == "add_buttons":
+                    keyboard = VkKeyboard(inline=False, one_time=False)
+                    keyboard.add_button(
+                        label="Зашёл",
+                        color=VkKeyboardColor.POSITIVE
+                    )
+                    keyboard.add_button(
+                        label="Перезахожу",
+                        color=VkKeyboardColor.PRIMARY
+                    )
+                    keyboard.add_button(
+                        label="Вышел",
+                        color=VkKeyboardColor.NEGATIVE
+                    )
+
+                    vk_session.method("messages.send", {
+                        "chat_id": chat_id,
+                        "message": "Секунду...",
+                        "keyboard": keyboard.get_empty_keyboard(),
+                        "random_id": 0
+                    })
+                    vk_session.method("messages.send", {
+                        "chat_id": chat_id,
+                        "message": "Успешно!",
+                        "keyboard": keyboard.get_keyboard(),
+                        "random_id": 0
+                    })
+
+    except Exception as error:
+        print(error)
